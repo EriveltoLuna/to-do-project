468db9d035f7e6ca6fa099ed3c2999c5f8c5cb89
Velto <eriveltoferreiralunafilho@gmail.com>
2024-10-22 11:27:02 -0300

    added host

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose
     .connect(process.env.URI)
     .then(() => {
         console.log('*** connected to database!');
-        app.listen(port, () => {
+        app.listen(port, '0.0.0.0', () => {
             console.log('*** server is running on PORT');
         });
     })
